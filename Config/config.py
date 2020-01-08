4837d2bfc18bb60d85ce6060c425ff059f344dc6
Yossef Azoulay <louis6legros@protonmail.com>
2020-01-08 14:01:23 +0200

    add some parameters in config.yaml and rename the main file

--- Config/config.py
+++ Config/config.py
@@ -1,17 +1,15 @@
 import yaml
 
-
-
 class Config(object):
     def __init__(self):
-        self._config = conf # set it to conf
+        self._config = self.load_config()
         
     def load_config() :
         with open("config.yml", 'r') as ymlfile:
             cfg = yaml.load(ymlfile)
 
     def get_property(self, property_name):
-        if property_name not in self._config.keys(): # we don't want KeyError
-            return None  # just return None if not found
+        if property_name not in self._config.keys(): 
+            return None
         return self._config[property_name]
     
