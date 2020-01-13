6072625397bb54ae0ecf59dc4af1b229e3e4cf36
Yossef Azoulay <louis6legros@protonmail.com>
2020-01-13 12:23:56 +0200

    add ftp_date_one method to check

--- Factory/FtpLoader.py
+++ Factory/FtpLoader.py
@@ -4,13 +4,11 @@ from ftplib import FTP
 
 class FtpLoader:
 
-    __slots__ = ['hostname', 'port', 'filename', 'ftp']
+    __slots__ = ['hostname', 'port', 'ftp']
 
     def __init__(self, hostname, filename=None, port=21):
-
         self.hostname = hostname
         self.port = port
-        self.filename = filename
 
     def connect(self):
         ftp = FTP()
@@ -25,6 +23,15 @@ class FtpLoader:
         except:  # ftplib.error_perm
             raise Exception('Authentication to {} failed'.format(self.hostname))
 
+    def ftp_date_all(self):
+        dir_listing = []
+        self.ftp.dir(lambda x: dir_listing.append(x))
+        return [(line.split(' ')[0], line.split(' ')[-1]) for line in dir_listing]
+
+    def ftp_date_one(self, filename):
+        dir_listing = []
+        self.ftp.dir(lambda x: dir_listing.append(x))
+        return [(line.split(' ')[0], line.split(' ')[-1]) for line in dir_listing if line.split(' ')[-1] == filename][0]
 
     def get(self, file):
         return self.ftp.retrbinary('RETR ' + file, open('%s/%s' % (self.hostname, file), 'wb').write)
--- bootstrap.py
+++ bootstrap.py
@@ -4,6 +4,8 @@ from Factory.FtpLoader import FtpLoader
 if __name__ == '__main__' :
 
     config = Config()
-    ftpload = FtpLoader(config.get_property('URL').get('MOT'), config.get_property('FILES').get('TRANSPORTATION') )
+    ftpload = FtpLoader(config.get_property('URL').get('MOT'))
     ftpload.connect()
+    list = ftpload.ftp_date_one(config.get_property('FILES').get('TRANSPORTATION'))
+    print(list)
 
