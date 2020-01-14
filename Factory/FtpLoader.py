43abd0ecf343be1a2d9a670ce1d6b6b3a62f707f
Yossef Azoulay <louis6legros@protonmail.com>
2020-01-14 07:30:35 +0200

    add check for download path exist and create it if not

--- Factory/FtpLoader.py
+++ Factory/FtpLoader.py
@@ -3,6 +3,7 @@ from utils.path import *
 import threading
 import logging
 from ftplib import FTP
+from pathlib import Path
 import socket
 import time
 import os
@@ -108,7 +109,9 @@ class FtpLoader(baseClass):
 
         local_filename = dst_filename
         if outDir is not None:
-            local_filename = os.path.join(GetParentDir(os.path.dirname(__file__)), outDir, local_filename)
+            checkPath = os.path.join(GetParentDir(os.path.dirname(__file__)), outDir)
+            Path(checkPath).mkdir(parents=True, exist_ok=True)
+            local_filename = os.path.join(checkPath, local_filename)
         try:
             conn = self.connect()
         except:
