a23a813563faf88a97e81368a18f8358541bdebd
Yossef Azoulay <louis6legros@protonmail.com>
2020-01-16 14:26:55 +0200

    extract zip file

--- Model/geoCsv.py
+++ Model/geoCsv.py
@@ -8,25 +8,27 @@ import os
 
 
 @timing
-def mainGeo() :
+def checkPointsFromFile(dir, filename) :
     JERUSALEM = getJerusalemBorder()
-    workFile = os.path.join(GetParentDir(os.path.dirname(__file__)), 'download/israel-public-transportation/stops.txt')
+    workFile = os.path.join(GetParentDir(os.path.dirname(__file__)), dir, filename)
     with open(workFile) as f :
         listofPoitn = []
-        i = 0
-        j = 0
+        i = -1
         for chunk in read_in_chunks(f, 2048) :
-            Current_route_points = []
-            Current_route_id = -1
-            Current_route_intersect = False
             for p in filter(None, chunk.split('\n')) :
+                if i == -1 :
+                    columns = p.split(',')
+                    id_index = [i for i, s in enumerate(columns) if 'id' in s][0]
+                    lat_index = [i for i, s in enumerate(columns) if 'lat' in s][0]
+                    lon_index = [i for i, s in enumerate(columns) if 'lon' in s][0]
+                    i = 0
                 try:
                     point = p.split(',')
-                    pointCheck = Point(float(point[4]), float(point[5]))
-                    chechPointWithinPolygonList(pointCheck, JERUSALEM) and listofPoitn.append([point[-1], point[0], pointCheck])
+                    pointCheck = Point(float(point[lat_index]), float(point[lon_index]))
+                    chechPointWithinPolygonList(pointCheck, JERUSALEM) and listofPoitn.append([point[id_index], point[0], pointCheck])
                 except :
-                    i += 1
                     continue
-        print (i)
         print(len(listofPoitn))
         print(listofPoitn)
+
+checkPointsFromFile('download/israel-public-transportation', 'stops.txt')
--- bootstrap.py
+++ bootstrap.py
@@ -3,20 +3,24 @@ from Configuration.config import Config
 import logging
 from logger.boot import setupLogging
 from Controller.FtpLoader import FtpLoader
-from Model.geoCsv import mainGeo
-
+from Model.geoCsv import *
+import zipfile
 
 if __name__ == '__main__' :
-    mainGeo()
-    # setupLogging()
-    # logger = logging.getLogger(__name__)
-    #
-    # config = Config()
-    # urls = config.get_property("URL")
-    # for domain in urls :
-    #     for dom, url in domain.items() :
-    #         ftp = FtpLoader(url)
-    #         download_dir = config.get_property("WS").get("DOWNLOAD").get(dom)
-    #         for file in config.get_property("FILES").get(dom) :
-    #             ftp.downloadFileItem(file, outDir=download_dir)
+
+    setupLogging()
+    logger = logging.getLogger(__name__)
+
+    config = Config()
+    urls = config.get_property("URL")
+    for domain in urls :
+        for dom, url in domain.items() :
+            ftp = FtpLoader(url)
+            download_dir = config.get_property("WS").get("DOWNLOAD").get(dom)
+            for file in config.get_property("FILES").get(dom) :
+                ftp.downloadFileItem(file, outDir=download_dir)
+                with zipfile.ZipFile(os.path.join(download_dir, file), 'r') as zip_ref:
+                    zip_ref.extractall(download_dir)
+                checkPointsFromFile(download_dir, file[:-3])
+
 
